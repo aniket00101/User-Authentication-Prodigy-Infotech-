f072f01fa00c44b4d0692f3f81e6f35d2f0b33b1
aniket00101 <dasaniket971@gmail.com>
2025-09-04 18:36:38 +0530

    backend completed

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,8 +1,16 @@
 import express from "express"
-import { registerUser } from "../controllers/userController.js"
+import { changePassword, forgotPassword, loginUser, logout, registerUser, verification, verifyOtp } from "../controllers/userController.js"
+import { isAuthenticated } from "../middleware/isAuthenticated.js"
+import { userSchema, validateUser } from "../validators/userValidate.js"
 
 const router = express.Router()
 
-router.post('/register', registerUser)
+router.post('/register', validateUser(userSchema), registerUser)
+router.post('/verify', verification)
+router.post('/login', loginUser)
+router.post('/logout', isAuthenticated, logout)
+router.post('/forgot-password', forgotPassword)
+router.post('/verify-otp/:email', verifyOtp)
+router.post('/change-password/:email', changePassword)
 
 export default router
